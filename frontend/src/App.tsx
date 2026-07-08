fc8d55b78f8e523ef738449ae4c576d991b0f299
Tester <t@t.test>
2026-07-08 20:36:59 +0200

    feat(agent-mail): add AgentMailPage, help dialog, and route/nav wiring

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -28,6 +28,7 @@ const PlansPage = lazy(() => import('./features/plans/PlansPage').then((m) => ({
 const PlanDetailPage = lazy(() => import('./features/plans/PlanDetailPage').then((m) => ({ default: m.PlanDetailPage })))
 const CCBridgePage = lazy(() => import('./features/cc-bridge/CCBridgePage').then((m) => ({ default: m.CCBridgePage })))
 const PresencePage = lazy(() => import('./features/presence/PresencePage').then((m) => ({ default: m.PresencePage })))
+const AgentMailPage = lazy(() => import('./features/agent-mail/AgentMailPage').then((m) => ({ default: m.AgentMailPage })))
 const ScheduledMessagesPage = lazy(() => import('./features/scheduled-messages/ScheduledMessagesPage').then((m) => ({ default: m.ScheduledMessagesPage })))
 const KanbanPage = lazy(() => import('./features/kanban/KanbanPage'))
 const ApmPage = lazy(() => import('./features/apm/ApmPage').then((m) => ({ default: m.ApmPage })))
@@ -68,6 +69,7 @@ function App() {
                 <Route path="agent-bridge" element={<CCBridgePage />} />
                 <Route path="cc-bridge" element={<CCBridgePage />} />
                 <Route path="presence" element={<PresencePage />} />
+                <Route path="agent-mail" element={<AgentMailPage />} />
                 <Route path="scheduled-messages" element={<ScheduledMessagesPage />} />
                 <Route path="plans/:filename" element={<PlanDetailPage />} />
                 <Route path="plans" element={<PlansPage />} />
